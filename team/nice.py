3e8d3edefa55f38a6d0d71dfaaff6fc7fbcb325e
niceputt <niceputri18@gmail.com>
2025-02-20 16:16:02 +0700

    Update nice.py

--- team/nice.py
+++ team/nice.py
@@ -1,1 +1,10 @@
+def perkenalan (nama, nim, peran):
+  print("Halo! Perkenalkan Nama Saya", nama)
+  print("NIM :", nim)
+  print("Peran saya yaitu :", peran)
 
+if __name__ == "__main__":
+  nama = "Nice Putri Mentari"
+  nim = "2213010431"
+  peran = "mengerjakan form crud"
+  perkenalan(nama, nim, peran)
